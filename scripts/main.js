48a9ed8dd820a112fc5be78c1bb85e1105f3ed10
JOJO19 <31725457+JoshuaPoddoku@users.noreply.github.com>
2018-12-18 02:29:55 +0530

    dashboard added

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,21 @@
-document.getElementById("demo").innerHTML = "Screen width is " + screen.width;
+
+$('.clickMe').click(function () {
+    "use strict";
+    $(this).hide();
+    $('#' + $(this).attr('for'))
+                    .val($(this).text())
+                    .toggleClass("form-control")
+                    .show()
+                    .focus();
+});
+
+$('.blur').blur(function () {
+    "use strict";
+    $(this)
+        .hide()
+        .toggleClass("form-control");
+    var myid = (this).id;
+    $('span[for=' + myid + ']')
+        .text($(this).val())
+        .show();
+});
